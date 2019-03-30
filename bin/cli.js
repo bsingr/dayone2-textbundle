f4f6cc7574cb13744f1d706f46e59859800532c6
Jens Bissinger <mail@jens-bissinger.de>
2019-03-30 17:10:29 +0100

    Use uuid in filename to avoid conflicts and improve logging

--- bin/cli.js
+++ bin/cli.js
@@ -15,25 +15,35 @@ if (!inputPath || !outputPath) {
   console.log(`${process.argv0} <dayone2-export-json-file> <target-dir>`)
 } else {
   const dayOne2JSONExport = parseDayOne2JSONExport(inputPath)
+  console.log(`dayOne2JSONExport.entries ${dayOne2JSONExport.entries.length}`)
+  console.log(`dayOne2JSONExport.entriesWithErrors ${dayOne2JSONExport.entriesWithErrors.length}`)
   dayOne2JSONExport.entriesWithErrors.forEach(async entryWithError => {
-    console.log(`${entryWithError.error} ${JSON.stringify(entryWithError)}`)
+    console.log(`${entryWithError.error} ${entryWithError.rawEntry.uuid} ${(entryWithError.rawEntry.attachments || []).length}`)
   })
-  dayOne2JSONExport.entries.forEach(async entry => {
-    const zip = buildTextBundleZip(entry)
-    const data = await zip.generateAsync({"type": 'nodebuffer'})
-    const path = outputPath + '/' + filenamify(entry.title) + '.textpack'
-    fs.writeFileSync(path, data)
-    try {
-      await new Promise((resolve) => {
+  Promise.all(dayOne2JSONExport.entries.map(entry => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const zip = buildTextBundleZip(entry)
+        const data = await zip.generateAsync({"type": 'nodebuffer'})
+        const path = `${outputPath}/${filenamify(entry.title)}.${entry.uuid}.textpack`
+        fs.writeFileSync(path, data)
+        
         const createdAt = new Date(entry.createdAt).getTime() || undefined
         const modifiedAt = new Date(entry.modifiedAt).getTime() || undefined
         const accessedAt = undefined
-        Utimes.utimes(path, createdAt, createdAt, accessedAt, () => {
-          resolve()
+        Utimes.utimes(path, createdAt, createdAt, createdAt, (error) => {
+          if (error) {
+            console.log(error)
+            reject(error)
+          } else {
+            resolve()
+          }
         })
-      })
-    } catch (e) {
-      console.log(e)
-    }
-  })
+      } catch (e) {
+        console.log(e)
+      }
+    })
+  }))
+  .then(tasks => console.log(`DONE ${tasks.length}`))
+  .catch(e => console.log(`Failed ${e}`))
 }
