c0dab121a949db4f45bbfe3c7da2db03b69317ce
Jens Bissinger <mail@jens-bissinger.de>
2019-03-31 10:58:41 +0200

    Refactor and test bundle file writer; fixed bug

--- bin/cli.js
+++ bin/cli.js
@@ -2,11 +2,7 @@
 
 /* eslint-disable */
 
-const parseDayOne2JSONExport = require('../lib/parseDayOne2JSONExport')
-const buildTextBundleZip = require('../lib/buildTextBundleZip')
-const fs = require('fs')
-const filenamify = require('filenamify');
-const Utimes = require('@ronomon/utimes');
+const convertDayOne2JSONExportIntoTextBundleZips = require('../lib/convertDayOne2JSONExportIntoTextBundleZips')
 
 const inputPath = process.argv[2]
 const outputPath = process.argv[3]
@@ -14,36 +10,5 @@ const outputPath = process.argv[3]
 if (!inputPath || !outputPath) {
   console.log(`${process.argv0} <dayone2-export-json-file> <target-dir>`)
 } else {
-  const dayOne2JSONExport = parseDayOne2JSONExport(inputPath)
-  console.log(`dayOne2JSONExport.entries ${dayOne2JSONExport.entries.length}`)
-  console.log(`dayOne2JSONExport.entriesWithErrors ${dayOne2JSONExport.entriesWithErrors.length}`)
-  dayOne2JSONExport.entriesWithErrors.forEach(async entryWithError => {
-    console.log(`${entryWithError.error} ${entryWithError.rawEntry.uuid} ${(entryWithError.rawEntry.attachments || []).length}`)
-  })
-  Promise.all(dayOne2JSONExport.entries.map(entry => {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const zip = buildTextBundleZip(entry)
-        const data = await zip.generateAsync({"type": 'nodebuffer'})
-        const path = `${outputPath}/${filenamify(entry.title)}.${entry.uuid}.textpack`
-        fs.writeFileSync(path, data)
-        
-        const createdAt = new Date(entry.createdAt).getTime() || undefined
-        const modifiedAt = new Date(entry.modifiedAt).getTime() || undefined
-        const accessedAt = undefined
-        Utimes.utimes(path, createdAt, modifiedAt, accessedAt, (error) => {
-          if (error) {
-            console.log(error)
-            reject(error)
-          } else {
-            resolve()
-          }
-        })
-      } catch (e) {
-        console.log(e)
-      }
-    })
-  }))
-  .then(tasks => console.log(`DONE ${tasks.length}`))
-  .catch(e => console.log(`Failed ${e}`))
+  convertDayOne2JSONExportIntoTextBundleZips(inputPath, outputPath)
 }
---
+++ test/lib/convertDayOne2JSONExportIntoTextBundleZipsTest.js
@@ -0,0 +1,71 @@
+const assert = require('assert')
+const convertDayOne2JSONExportIntoTextBundleZips = require('../../lib/convertDayOne2JSONExportIntoTextBundleZips')
+const rimraf = require('rimraf')
+const fs = require('fs')
+const JSZip = require('jszip');
+
+describe('convertDayOne2JSONExportIntoTextBundleZips', () => {
+  describe('normal', () => {
+    it('creates files with proper names, timestamps and contents', async () => {
+      // convert
+      const targetDir = './test/tmp/convertDayOne2JSONExportIntoTextBundleZips'
+      rimraf.sync(targetDir)
+      fs.mkdirSync(targetDir)
+      const logs = await convertDayOne2JSONExportIntoTextBundleZips(
+        __dirname + '/../assets/DayOne2JSONExport/demo.json', targetDir
+      )
+
+      // check logs
+      assert.deepEqual(logs, [
+        './test/tmp/convertDayOne2JSONExportIntoTextBundleZips/Initial entry.F4CF0509F3EA47D1B56F95D37F165F5E.textpack',
+        './test/tmp/convertDayOne2JSONExportIntoTextBundleZips/Second entry.6BD0AE9E21C447A6BC504A63899BA543.textpack'
+      ])
+
+      // files are created with proper filenames
+      assert.deepEqual(fs.readdirSync(targetDir), [
+        'Initial entry.F4CF0509F3EA47D1B56F95D37F165F5E.textpack',
+        "Second entry.6BD0AE9E21C447A6BC504A63899BA543.textpack"
+      ])
+
+      // file timestamps are correctly set
+      const statSubset = ({birthtime, mtime}) => ({birthtime, mtime})
+      assert.deepEqual(statSubset(fs.statSync(`${targetDir}/Initial entry.F4CF0509F3EA47D1B56F95D37F165F5E.textpack`)), {
+        birthtime: new Date('2019-03-18T13:04:56.000Z'),
+        mtime: new Date('2019-03-26T21:48:44.000Z')
+      })
+      assert.deepEqual(statSubset(fs.statSync(`${targetDir}/Second entry.6BD0AE9E21C447A6BC504A63899BA543.textpack`)), {
+        birthtime: new Date('2010-02-26T21:44:20.000Z'),
+        mtime: new Date('2010-03-26T21:48:55.000Z')
+      })
+
+      // file1 content is correct
+      const zip1 = await JSZip.loadAsync(fs.readFileSync(`${targetDir}/Initial entry.F4CF0509F3EA47D1B56F95D37F165F5E.textpack`))
+      assert.deepEqual(await zip1.file("info.json").async("string"), `{
+  "version": 2,
+  "type": "net.daringfireball.markdown",
+  "transient": true,
+  "creatorIdentifier": "org.dayoneexport.vendor",
+  "sourceURL": "dayone2://view?entryId=F4CF0509F3EA47D1B56F95D37F165F5E"
+}`)
+      assert.deepEqual(await zip1.file("text.md").async("string"), `# Initial entry
+
+some text
+
+![](assets/cf80411fda6ba991b6110b2365fb8286.jpeg)`)
+      assert.equal(await zip1.file('assets/cf80411fda6ba991b6110b2365fb8286.jpeg').name, 'assets/cf80411fda6ba991b6110b2365fb8286.jpeg')
+
+      // file2 content is correct
+      const zip2 = await JSZip.loadAsync(fs.readFileSync(`${targetDir}/Second entry.6BD0AE9E21C447A6BC504A63899BA543.textpack`))
+      assert.deepEqual(await zip2.file("info.json").async("string"), `{
+  "version": 2,
+  "type": "net.daringfireball.markdown",
+  "transient": true,
+  "creatorIdentifier": "org.dayoneexport.vendor",
+  "sourceURL": "dayone2://view?entryId=6BD0AE9E21C447A6BC504A63899BA543"
+}`)
+      assert.deepEqual(await zip2.file("text.md").async("string"), `# Second entry
+
+some text`)
+    })
+  })
+})
